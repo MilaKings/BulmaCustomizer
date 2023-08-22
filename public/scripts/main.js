2cd313c98c8e18b26e0f66801bdc9961791e413a
Mila Kings <mila.dreis@gmail.com>
2023-08-21 23:25:17 -0300

    Add new functionalities and fix formatting

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,48 +1,72 @@
 const compileButton = document.getElementById('compileButton');
-const sassInput = document.getElementById('sassInput');
+//const sassInput = document.getElementById('sassInput');
 const outputDiv = document.getElementById('output');
 const testCSSCheckBox = document.querySelectorAll('.testarCSS');
 const stylesheetLink = document.getElementById('stylesheet');
 
 testCSSCheckBox[1].addEventListener('change', () => {
-    let currentStylesheet = 'bulma.css';
-    const alternativeStylesheet = './temp/compiled.css';
+  let currentStylesheet = 'bulma.css';
+  const alternativeStylesheet = './temp/compiled.css';
 
-    if (testCSSCheckBox[1].checked) {
-        stylesheetLink.href = alternativeStylesheet;
-        currentStylesheet = alternativeStylesheet;
-    } else {
-        stylesheetLink.href = currentStylesheet;
-        currentStylesheet = currentStylesheet;
-    }
+  if (testCSSCheckBox[1].checked) {
+    stylesheetLink.href = alternativeStylesheet;
+    currentStylesheet = alternativeStylesheet;
+  } else {
+    stylesheetLink.href = currentStylesheet;
+    currentStylesheet = currentStylesheet;
+  }
 });
 
 compileButton.addEventListener('click', async () => {
-    try {
-        compileButton.classList.add('is-loading');
-        const inputCode = sassInput.value;
+  try {
+    compileButton.classList.add('is-loading');
+    let bulmaVariables = document.querySelectorAll('.bulma-variable');
+    let sassString = '';
+    let importUrlString = '';
+    let urlFont = '';
+    bulmaVariables.forEach(item => {
+      if (item.id === "@import url") {
+        importUrlString = `@import url('${item.value}');\n`;
+        urlFont = item.value;
+      } else {
+        sassString += `
+          ${item.id}: ${item.value};`;
+      }
+    });
 
-        const response = await fetch('/compile-sass', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ inputCode })
-        });
-
-        const cssContent = await response.text();
-        compileButton.classList.remove('is-loading');
-        testCSSCheckBox[1].checked = false;
-
-        const blob = new Blob([cssContent], { type: 'text/css' });
-        const url = URL.createObjectURL(blob);
-
-        outputDiv.href = url;
-        outputDiv.download = 'compiled.css';
-
-        ['disabled', 'title'].forEach(attribute => outputDiv.removeAttribute(attribute));
-        testCSSCheckBox.forEach(item => { item.removeAttribute('disabled') });
-    } catch (error) {
-        console.error('Erro ao compilar Sass:', error);
+    urlFont = extractFontNameFromUrl(urlFont);
+    if (importUrlString && urlFont) {
+      sassString = `${importUrlString} ${sassString}
+      $family-sans-serif: ${urlFont}, sans-serif;
+      `;
     }
+    console.log(sassString);
+    const response = await fetch('/compile-sass', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ sassString })
+    });
+
+    const cssContent = await response.text();
+    compileButton.classList.remove('is-loading');
+    testCSSCheckBox[1].checked = false;
+
+    const blob = new Blob([cssContent], { type: 'text/css' });
+    const url = URL.createObjectURL(blob);
+
+    outputDiv.href = url;
+    outputDiv.download = 'compiled.css';
+
+    ['disabled', 'title'].forEach(attribute => outputDiv.removeAttribute(attribute));
+    testCSSCheckBox.forEach(item => { item.removeAttribute('disabled') });
+  } catch (error) {
+    console.error('Erro ao compilar Sass:', error);
+  }
 });
+
+function extractFontNameFromUrl(url) {
+  const fontNameRegex = /family=([^&:]+)/;
+  const match = url.match(fontNameRegex);
+
+  return (match && match[1]) ? match[1] : false;
+}
